283a2226289736efaeea4391183793d473898095
Gabriel <97975826+Xishie@users.noreply.github.com>
2023-10-26 08:45:54 +0200

    Adding php file

---
+++ php-simple/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>cargo-demo</title>
+</head>
+<body>
+    <h1>Welcome to deez</h1>
+
+    <?php
+    echo "Today is " . date("d.m.Y") . "<br>";
+    echo "It currently is " . date("h:i a") . "<br>";
+    echo "This site is running on" . "<br>";
+    echo gethostname();
+    ?>
+    
+
+</body>
+</html>
